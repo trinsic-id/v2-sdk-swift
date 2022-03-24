6faf910343cf0fb38016821b86b2df3b4935c176
Scott Phillips <polygonguru@gmail.com>
2022-03-23 22:53:51 -0400

    SDK 504

--- Sources/Trinsic/WalletService.swift
+++ Sources/Trinsic/WalletService.swift
@@ -22,7 +22,7 @@ public class WalletService : ServiceBase
         client = Services_Universalwallet_V1_UniversalWalletClient(channel: createChannel())
     }
     
-    public func search(query: String = "SELECT * from c") throws -> Services_Universalwallet_V1_SearchResponse {
+    public func search(query: String = "SELECT c.id, c.type, c.data FROM c") throws -> Services_Universalwallet_V1_SearchResponse {
         var request = Services_Universalwallet_V1_SearchRequest();
         request.query = query;
         
