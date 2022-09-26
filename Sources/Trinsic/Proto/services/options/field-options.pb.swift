0a95f24810bdef432a4bf77380c1a95e4d0f462e
Scott Phillips <polygonguru@gmail.com>
2022-09-26 15:44:46 -0400

    update generated protos

--- Sources/Trinsic/Proto/services/options/field-options.pb.swift
+++ Sources/Trinsic/Proto/services/options/field-options.pb.swift
@@ -27,34 +27,19 @@ public struct Services_Options_SdkTemplateOption {
 
     /// Whether the service endpoint allows anonymous (no auth token necessary) authentication
     /// This is used by the `protoc-gen-trinsic-sdk` plugin for metadata.
-    public var anonymous: Bool {
-        get { return _anonymous ?? false }
-        set { _anonymous = newValue }
-    }
-
-    /// Returns true if `anonymous` has been explicitly set.
-    public var hasAnonymous: Bool { return _anonymous != nil }
-    /// Clears the value of `anonymous`. Subsequent reads from it will return its default value.
-    public mutating func clearAnonymous() { _anonymous = nil }
+    public var anonymous: Bool = false
 
     /// Whether the SDK template generator should ignore this method. This method will
     /// be wrapped manually.
-    public var ignore: Bool {
-        get { return _ignore ?? false }
-        set { _ignore = newValue }
-    }
+    public var ignore: Bool = false
 
-    /// Returns true if `ignore` has been explicitly set.
-    public var hasIgnore: Bool { return _ignore != nil }
-    /// Clears the value of `ignore`. Subsequent reads from it will return its default value.
-    public mutating func clearIgnore() { _ignore = nil }
+    /// Whether the SDK template generator should generate this method without arguments, eg
+    /// ProviderService.GetEcosystemInfo() where the request object is empty
+    public var noArguments: Bool = false
 
     public var unknownFields = SwiftProtobuf.UnknownStorage()
 
     public init() {}
-
-    fileprivate var _anonymous: Bool?
-    fileprivate var _ignore: Bool?
 }
 
 #if swift(>=5.5) && canImport(_Concurrency)
@@ -148,6 +133,7 @@ extension Services_Options_SdkTemplateOption: SwiftProtobuf.Message, SwiftProtob
     public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
         1: .same(proto: "anonymous"),
         2: .same(proto: "ignore"),
+        3: .standard(proto: "no_arguments"),
     ]
 
     public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
@@ -156,30 +142,31 @@ extension Services_Options_SdkTemplateOption: SwiftProtobuf.Message, SwiftProtob
             // allocates stack space for every case branch when no optimizations are
             // enabled. https://github.com/apple/swift-protobuf/issues/1034
             switch fieldNumber {
-            case 1: try try decoder.decodeSingularBoolField(value: &_anonymous)
-            case 2: try try decoder.decodeSingularBoolField(value: &_ignore)
+            case 1: try try decoder.decodeSingularBoolField(value: &anonymous)
+            case 2: try try decoder.decodeSingularBoolField(value: &ignore)
+            case 3: try try decoder.decodeSingularBoolField(value: &noArguments)
             default: break
             }
         }
     }
 
     public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
-        // The use of inline closures is to circumvent an issue where the compiler
-        // allocates stack space for every if/case branch local when no optimizations
-        // are enabled. https://github.com/apple/swift-protobuf/issues/1034 and
-        // https://github.com/apple/swift-protobuf/issues/1182
-        try { if let v = self._anonymous {
-            try visitor.visitSingularBoolField(value: v, fieldNumber: 1)
-        } }()
-        try { if let v = self._ignore {
-            try visitor.visitSingularBoolField(value: v, fieldNumber: 2)
-        } }()
+        if anonymous != false {
+            try visitor.visitSingularBoolField(value: anonymous, fieldNumber: 1)
+        }
+        if ignore != false {
+            try visitor.visitSingularBoolField(value: ignore, fieldNumber: 2)
+        }
+        if noArguments != false {
+            try visitor.visitSingularBoolField(value: noArguments, fieldNumber: 3)
+        }
         try unknownFields.traverse(visitor: &visitor)
     }
 
     public static func == (lhs: Services_Options_SdkTemplateOption, rhs: Services_Options_SdkTemplateOption) -> Bool {
-        if lhs._anonymous != rhs._anonymous { return false }
-        if lhs._ignore != rhs._ignore { return false }
+        if lhs.anonymous != rhs.anonymous { return false }
+        if lhs.ignore != rhs.ignore { return false }
+        if lhs.noArguments != rhs.noArguments { return false }
         if lhs.unknownFields != rhs.unknownFields { return false }
         return true
     }
